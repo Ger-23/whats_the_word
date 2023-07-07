cc8b77258a460081a8adb132df60f195178d5cf5
Ger-23 <>
2023-07-07 16:07:27 +0000

    Define function to show dashes for letters that have not been guessed and reveals letters that have been guessed

--- run.py
+++ run.py
@@ -12,14 +12,29 @@ MAX_LIVES = 5
 
 def choose_word():
     """
-    Chooses random word from word list and returns a word from said list, in all caps.
+    Choose random word from word list and returns a word from said list, in all caps.
     """
     random_word_index = random.randint(0, len(WORD_LIST) -1)
     return WORD_LIST[random_word_index].upper()
 
-random_word = choose_word()
-print("Random word:", random_word)
 
+def dashed_word_rep(word, guessed_letters):
+    """
+    Print dashed representation of chosen word.
+    """
+    dashed_word = []
+    for letter in word:
+        if letter in guessed_letters:
+            dashed_word.append(letter)
+        else:
+            dashed_word.append('_')
+
+    print(' '.join(dashed_word))
+    
+test_word = "HOUSE"
+guessed_letters = ["O", "E"]
+print(test_word, guessed_letters)
+dashed_word_rep(test_word, guessed_letters)
 
 #def wtw():
 
