cbc8fdc4a808ec6b36e3dc9cda46e9f7f3018fb5
Ger-23 <>
2023-07-07 15:32:34 +0000

    Defined constant variables and function to generate a random word from list

--- run.py
+++ run.py
@@ -1,5 +1,26 @@
 import random
 
-def wtw():
-    print("Welcome to What's The Word")
-wtw()
+WORD_LIST = [
+    "Homer",
+    "Apu",
+    "Smithers",
+    "Flanders",
+    "Barney"
+]
+
+MAX_LIVES = 5
+
+def choose_word():
+    """
+    Chooses random word from word list and returns a word from said list, in all caps.
+    """
+    random_word_index = random.randint(0, len(WORD_LIST) -1)
+    return WORD_LIST[random_word_index].upper()
+
+random_word = choose_word()
+print("Random word:", random_word)
+
+
+#def wtw():
+
+#wtw()
